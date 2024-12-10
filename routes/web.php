e9fb090d22eef1c21931a1a9fd46d3895485a45b
Soumitra Chakrabartee <soumitra.jr@gmail.com>
2024-12-10 10:35:54 +0600

    migrate file update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/migrate', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate --force');
+    return response()->json(['message' => 'Migrations run successfully!']);
+});
