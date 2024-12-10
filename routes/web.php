c07c6657e3fcad0bb6214de3acb6fe47c209fcfe
Soumitra Chakrabartee <soumitra.jr@gmail.com>
2024-12-10 13:05:57 +0600

    Create DB Added on Web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,4 +9,14 @@ Route::get('/', function () {
 Route::get('/migrate', function () {
     \Illuminate\Support\Facades\Artisan::call('migrate:refresh --force');
     return response()->json(['message' => 'Migrations run successfully!']);
+});
+
+Route::get('/create-database/{name}', function ($name) {
+    try {
+        DB::statement("CREATE DATABASE $name");
+
+        return "Database '$name' created successfully!";
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
 });
